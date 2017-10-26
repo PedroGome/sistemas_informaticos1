8fd989e916f923312241e0a75aa3dbd93f20d7c8
zentauro <pedrogomezmartin1999@gmail.com>
2017-10-26 17:15:15 +0200

    Ejercicio 10 inicial esqueleto

---
+++ Practica2/ejercicio10.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main(){
+  unsigned char car1;
+  char car2;
+
+  printf("Constantes y variables de tipo caracter\n");
+
+  car1 = 0x35 + 96 - ' ';                         /* (a) */
+  car2 = "z";                                     /* (b) */
+
+  printf("\nEjemplo 1:\n");                       /* (c) */
+  printf("Caracteres:\t%c\t%c\n",car1,car2);
+  printf("Valores:\t%d\t%d\n",car1,car2);
+
+  car1 = 0x9C;
+  car2 = car1;
+
+  printf("\nEjemplo 2:\n");
+  printf("Caracteres:\t%c\t%c\n",car1,car2);      /* (d) */
+  printf("Valores:\t%d\t%d\n",car1,car2);         /* (e) */
+
+  return 0;
+}
