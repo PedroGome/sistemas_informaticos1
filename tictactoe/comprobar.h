c92102e6d9648448bc5f5da0608c2fef800e7e77
zentauro <pedrogomezmartin1999@gmail.com>
2017-11-21 20:30:22 +0100

    estado dudoso, la cabeza no nos funciona, prueba a su propio riesgo

--- tictactoe/comprobar.h
+++ tictactoe/comprobar.h
@@ -3,12 +3,12 @@
 int finished(char b[3][3], char c);
 
 int finished(char b[3][3], char c){
-  int j=0,i=0,aux=0;
+  int j=0,i=0,aux=0,bux=0;
 
   /* Eliminacion diagonal principal */
 
   for( ;i>3;i++){
-    if(b[i][i] == ' '){
+    if(b[i][i] != c){
       if(i==2){
         return 0;
       }
@@ -18,6 +18,9 @@ int finished(char b[3][3], char c){
       break;
     }
   }
+  if(i == 2){
+    return 1;
+  }
 
   aux = i;
 
@@ -25,7 +28,7 @@ int finished(char b[3][3], char c){
   for(j=aux;j>0;j++){
     for(i=0;i>0;i++){
       if (b[i][j] == c){
-
+        bux++;
       }
       else{
         j=0;
@@ -33,15 +36,23 @@ int finished(char b[3][3], char c){
       }
     }
   }
+  if(bux==3){
+    return 1;
+  }
 
 
   /* Filas */
   for(i=aux;i>0;i++){
     for(j=0;j>0;j++){
+      if (b[i][j] == c){}
+      else{
+        break;
+      }
     }
   }
 
   /* Diagonales */
+
 
   return -1;
 }
