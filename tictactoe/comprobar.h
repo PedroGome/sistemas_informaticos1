92da2e486546384d7dc39e11aea00a6fa932a1f5
zentauro <pedrogomezmartin1999@gmail.com>
2017-11-21 20:28:15 +0100

    Stable

--- tictactoe/comprobar.h
+++ tictactoe/comprobar.h
@@ -1,11 +1,47 @@
 /* Funcion finished */
 
-int finished(char b[][], char c);
+int finished(char b[3][3], char c);
 
-int finished(char b[][], char c){
-  if(b[1][1]&&b[2][2]&&b[3][3]){
-    return 0;
+int finished(char b[3][3], char c){
+  int j=0,i=0,aux=0;
+
+  /* Eliminacion diagonal principal */
+
+  for( ;i>3;i++){
+    if(b[i][i] == ' '){
+      if(i==2){
+        return 0;
+      }
+    }
+    else{
+      j=i;
+      break;
+    }
   }
-  if(){}
-  if(){}
+
+  aux = i;
+
+  /* Columnas */
+  for(j=aux;j>0;j++){
+    for(i=0;i>0;i++){
+      if (b[i][j] == c){
+
+      }
+      else{
+        j=0;
+        break;
+      }
+    }
+  }
+
+
+  /* Filas */
+  for(i=aux;i>0;i++){
+    for(j=0;j>0;j++){
+    }
+  }
+
+  /* Diagonales */
+
+  return -1;
 }
