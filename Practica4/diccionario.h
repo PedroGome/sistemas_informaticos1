b69e912e9ee42e2d300643e8295353d4467f9cca
zentauro <pedrogomezmartin1999@gmail.com>
2017-12-13 12:59:20 +0100

    practica4 cambios

--- Practica4/diccionario.h
+++ Practica4/diccionario.h
@@ -10,8 +10,7 @@
 #define MAX_PAL 100
 #define MAX_CAD 20
 
-typedef struct
-{
+typedef struct {
   char ingles[MAX_CAD];
   char espanyol[MAX_CAD];
 } tPalabra;
@@ -21,3 +20,32 @@ void LeeCadena(char cadena[], int max);
 int anyadir_palabra (tPalabra Dicc[], int num);
 void traducir_palabra (tPalabra Dicc[], int num);
 void mostrar_diccionario (tPalabra Dicc[], int num);
+
+int anyadir_palabra (tPalabra *Dicc, int num){
+  if (num <= MAX_PAL){
+    printf("Diccionario lleno\n");
+    return num;
+  }
+
+  tPalabra dicc[MAX_PAL];
+  memcpy(dicc,Dicc,sizeof(dicc));
+
+  printf("Introducir espanyol: ");
+  LeeCadena(Dicc[num+1].espanyol, MAX_CAD);
+  printf("\nIntroducir ingles: ");
+  LeeCadena(Dicc[num+1].ingles, MAX_CAD);
+
+  memcpy(Dicc,dicc,sizeof(dicc));
+  return num+1;
+}
+
+void traducir_palabra (tPalabra Dicc[], int num){
+  char palabradedios[MAX_CAD];
+  LeeCadena(palabradedios, MAX_CAD);
+
+  for(int i=0; i<= num; i++){
+    if (!strcmp(palabradedios, Dicc[i].espanyol))
+      printf("Traduccion de %s es %s", palabradedios, Dicc[i].ingles);
+
+  }
+}
--- Practica4/main.c
+++ Practica4/main.c
@@ -3,7 +3,11 @@
 */
 
 #define MAX_CAD 20
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 #include "diccionario.h"
+#include "funcs.h"
 
 typedef struct{
   char ingles[MAX_CAD];
@@ -22,38 +26,4 @@ int main () {
 
 
 
-}
-
-int menu() {
-  int op;
-  do
-    {
-      system("cls");
-      printf ("\n"
-              "\t1.- Anyadir palabra\n"
-              "\t2.- Traducir palabra\n"
-              "\t3.- Mostrar diccionario\n"
-              "\t4.- Salir\n"
-              "\n"
-              "Elija una opcion: ");
-
-      scanf ("%d", &op);
-      while (getchar() != '\n'); // fflush(stdin);
-    }
-  while ((op<1)||(op>4));
-
-  return op;
-}
-
-void LeeCadena(char cadena[], int max) {
-	char *p;
-	int n;
-
-	p = fgets(cadena, max, stdin);
-	if (p != NULL)
-    {
-      n = strlen(cadena);
-      if (n>0 && cadena[n - 1] == '\n')
-        cadena[n - 1] = '\0';
-    }
 }
